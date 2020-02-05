d43f2caf6663ea9e1882e28fd10a2ce16a25cd67
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2020-02-05 17:33:22 +0300

    Add files via upload

---
+++ omd.py
@@ -0,0 +1,49 @@
+def step1():
+    location = input('Что будем делать - плавать или идти?')
+    if location == 'плавать':
+        print('Спасибо! Я люблю плавать :)')
+        step2()
+        quit()
+
+    if location == 'идти':
+        print('Что ж! Пойдем пешком..')
+        step3()
+        quit()
+
+    else:
+        print('Прости, я могу только идти или плавать. Напиши, что мне делать!')
+        step1()
+
+def step2():
+    player_answer = str(input('Ты умеешь плавать?'))
+    if player_answer == 'да':
+        print('Какая удача!')
+    elif player_answer == 'нет':
+        print('Чувствую, плыть мне сегодня одной..')
+    else:
+        print('отвечай "да" или "нет"')
+        step2()
+
+
+def step3():
+    small_talk = input('Как твои дела?')
+    print('Честно, я больше люблю плавать')
+    distance = int(input('Cколько километров еще идти?'))
+
+    if distance <= 1:
+        print('Думаю, я справлюсь! С тобой весело)) ')
+    elif distance > 1:
+        print('Далекооооо!!')
+    else:
+        raise Exception
+
+
+if __name__=='__main__':
+    print('Уточка решила погулять. ')
+    step1()
+
+
+
+
+
+
